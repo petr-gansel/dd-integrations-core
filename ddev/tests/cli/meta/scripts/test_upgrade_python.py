StingzLD/integrations-core

Improve the upgrade-python script

ddev/tests/cli/meta/scripts/test_upgrade_python.py
@@ -31,7 +31,7 @@ def test_upgrade_python(ddev, repository):
     result = ddev('meta', 'scripts', 'upgrade-python', new_version)
 
     assert result.exit_code == 0, result.output
-    assert result.output.startswith('Python upgrades\n\nPassed: ')
+    assert 'Python upgrades\n\nPassed: ' in result.output
 
     passed = int(result.output.partition('Passed:')[2].strip())
     assert passed >= minimum_changes
@@ -39,3 +39,34 @@ def test_upgrade_python(ddev, repository):
     contents = constant_file.read_text()
     assert f'PYTHON_VERSION = {PYTHON_VERSION!r}' not in contents
     assert f'PYTHON_VERSION = {new_version!r}' in contents
+
+    ci_file = repository.path / '.github' / 'workflows' / 'build-ddev.yml'
+    contents = ci_file.read_text()
+    assert f'PYTHON_VERSION: "{PYTHON_VERSION}"' not in contents
+    assert f'PYTHON_VERSION: "{new_version}"' in contents
+
+    hatch_file = repository.path / 'activemq' / 'hatch.toml'
+    contents = hatch_file.read_text()
+    assert f'python = ["{PYTHON_VERSION}"]' not in contents
+    assert f'python = ["{new_version}"]' in contents
+
+    pyproject_file = repository.path / 'activemq' / 'pyproject.toml'
+    contents = pyproject_file.read_text()
+    assert f'Programming Language :: Python :: {PYTHON_VERSION}' not in contents
+    assert f'Programming Language :: Python :: {new_version}' in contents
+
+    template_file = (
+        repository.path
+        / 'datadog_checks_dev'
+        / 'datadog_checks'
+        / 'dev'
+        / 'tooling'
+        / 'templates'
+        / 'integration'
+        / 'check'
+        / '{check_name}'
+        / 'pyproject.toml'
+    )
+    contents = template_file.read_text()
+    assert f'Programming Language :: Python :: {PYTHON_VERSION}' not in contents
+    assert f'Programming Language :: Python :: {new_version}' in contents

petr-gansel/dd-integrations-core

ddev/tests/cli/meta/scripts/test_upgrade_python.py
@@ -17,13 +17,39 @@ def test_upgrade_python(fake_repo, ddev):
     result = ddev('meta', 'scripts', 'upgrade-python', new_version)
 
     assert result.exit_code == 0, result.output
-    assert result.output == 'Python upgrades\n\nPassed: 2\n'
+    assert result.output.endswith('Python upgrades\n\nPassed: 7\n')
 
     contents = constant_file.read_text()
     assert f'PYTHON_VERSION = {old_version!r}' not in contents
     assert f'PYTHON_VERSION = {new_version!r}' in contents
 
+    ci_file = fake_repo.path / '.github' / 'workflows' / 'build-ddev.yml'
+    contents = ci_file.read_text()
+    assert f'PYTHON_VERSION: "{old_version}"' not in contents
+    assert f'PYTHON_VERSION: "{new_version}"' in contents
+
     hatch_file = fake_repo.path / 'dummy' / 'hatch.toml'
     contents = hatch_file.read_text()
     assert f'python = ["2.7", "{old_version}"]' not in contents
     assert f'python = ["2.7", "{new_version}"]' in contents
+
+    pyproject_file = fake_repo.path / 'dummy' / 'pyproject.toml'
+    contents = pyproject_file.read_text()
+    assert f'Programming Language :: Python :: {old_version}' not in contents
+    assert f'Programming Language :: Python :: {new_version}' in contents
+
+    template_file = (
+        fake_repo.path
+        / 'datadog_checks_dev'
+        / 'datadog_checks'
+        / 'dev'
+        / 'tooling'
+        / 'templates'
+        / 'integration'
+        / 'check'
+        / '{check_name}'
+        / 'pyproject.toml'
+    )
+    contents = template_file.read_text()
+    assert f'Programming Language :: Python :: {old_version}' not in contents
+    assert f'Programming Language :: Python :: {new_version}' in contents